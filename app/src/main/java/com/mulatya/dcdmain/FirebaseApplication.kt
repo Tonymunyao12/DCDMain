c86335e6ad433756306c754062806543437443bd
Mulatya <munyaotony12@gmail.com>
2022-03-29 19:44:12 +0300

    Dependency Provider using Kodein framework.

---
+++ app/src/main/java/com/mulatya/dcdmain/FirebaseApplication.kt
@@ -0,0 +1,25 @@
+package com.mulatya.dcdmain
+
+import android.app.Application
+import com.mulatya.dcdmain.data.firebase.FirebaseSource
+import com.mulatya.dcdmain.data.repositories.UserRepository
+import com.mulatya.dcdmain.ui.auth.AuthViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+
+class FirebaseApplication : Application(), KodeinAware {
+
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@FirebaseApplication))
+
+        bind() from singleton { FirebaseSource() }
+        bind() from singleton { UserRepository(instance()) }
+        bind() from singleton { AuthViewModelFactory(instance()) }
+    }
+
+
+}
