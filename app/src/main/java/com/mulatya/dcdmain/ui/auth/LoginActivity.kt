39ca4089314a3bd84ed87533ee3be7eed2364591
Mulatya <munyaotony12@gmail.com>
2022-03-30 20:24:56 +0300

    Binding the LoginActivity with the ViewModel using the Kodein Framework.

--- app/src/main/java/com/mulatya/dcdmain/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/mulatya/dcdmain/ui/auth/LoginActivity.kt
@@ -3,10 +3,20 @@ package com.mulatya.dcdmain.ui.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mulatya.dcdmain.R
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by Kodein()
+    private val factory : AuthViewModelFactory by instance()
+
+    private lateinit var viewModel: AuthViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
     }
 }
