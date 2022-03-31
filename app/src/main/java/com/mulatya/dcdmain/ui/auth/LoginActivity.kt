256059a90c800a05f7b3841408a7992d1fd32488
Mulatya <munyaotony12@gmail.com>
2022-03-31 19:57:45 +0300

    Binding the LoginActivity with the ViewModel.

--- app/src/main/java/com/mulatya/dcdmain/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/mulatya/dcdmain/ui/auth/LoginActivity.kt
@@ -2,14 +2,21 @@ package com.mulatya.dcdmain.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.mulatya.dcdmain.R
+import com.mulatya.dcdmain.databinding.ActivityLoginBinding
+import com.mulatya.dcdmain.utils.startHomeActivityMain
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
 import org.kodein.di.generic.instance
 
 class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
 
-    override val kodein by Kodein()
+    override val kodein by kodein()
     private val factory : AuthViewModelFactory by instance()
 
     private lateinit var viewModel: AuthViewModel
@@ -18,5 +25,32 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        viewModel = ViewModelProvider(this, factory).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
+
+    }
+
+    override fun onStarted() {
+
+    }
+
+    override fun onSuccess() {
+
+        startHomeActivityMain()
+    }
+
+    override fun onFailure(message: String) {
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.user?.let {
+            startHomeActivityMain()
+        }
     }
 }
