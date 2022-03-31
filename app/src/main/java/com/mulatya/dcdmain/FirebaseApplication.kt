da02988ae7d2801b04e7523449888cb73531faff
Mulatya <munyaotony12@gmail.com>
2022-03-31 20:12:58 +0300

    Class firebaseApplication which creates independence for classes.

--- app/src/main/java/com/mulatya/dcdmain/FirebaseApplication.kt
+++ app/src/main/java/com/mulatya/dcdmain/FirebaseApplication.kt
@@ -4,11 +4,13 @@ import android.app.Application
 import com.mulatya.dcdmain.data.firebase.FirebaseSource
 import com.mulatya.dcdmain.data.repositories.UserRepository
 import com.mulatya.dcdmain.ui.auth.AuthViewModelFactory
+import com.mulatya.dcdmain.ui.home_main.HomeViewModelFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
 class FirebaseApplication : Application(), KodeinAware {
@@ -18,7 +20,8 @@ class FirebaseApplication : Application(), KodeinAware {
 
         bind() from singleton { FirebaseSource() }
         bind() from singleton { UserRepository(instance()) }
-        bind() from singleton { AuthViewModelFactory(instance()) }
+        bind() from provider { AuthViewModelFactory(instance()) }
+        bind() from provider { HomeViewModelFactory(instance()) }
     }
 
 
