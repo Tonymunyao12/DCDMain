87c925c303223c974557f14b42d555a79ba7e2c7
Mulatya <munyaotony12@gmail.com>
2022-03-31 20:01:03 +0300

    Binding the SignupActivity with the AuthViewModel.

--- app/src/main/java/com/mulatya/dcdmain/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/mulatya/dcdmain/ui/auth/SignupActivity.kt
@@ -1,4 +1,52 @@
 package com.mulatya.dcdmain.ui.auth
 
-class SignupActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.mulatya.dcdmain.R
+import com.mulatya.dcdmain.databinding.ActivitySignupBinding
+import com.mulatya.dcdmain.ui.home_main.HomeActivityMain
+import com.mulatya.dcdmain.utils.startHomeActivityMain
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
+
+class SignupActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: AuthViewModelFactory by instance()
+
+    private lateinit var viewModel: AuthViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivitySignupBinding = DataBindingUtil.setContentView(this, R.layout.activity_signup)
+        viewModel = ViewModelProvider(this, factory).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        viewModel.authListener = this
+    }
+
+    override fun onStarted() {
+
+        Intent(this, HomeActivityMain::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+        }
+    }
+
+    override fun onSuccess() {
+     startHomeActivityMain()
+
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+    }
 }
